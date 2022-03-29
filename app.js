644cf144259a607e239698e6e2b78dab92fb4bb4
Arthur Noulas <arthur.noulas1@marist.edu>
2022-03-29 17:07:39 -0400

    reconfiguring

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path'); //created the path module
 const port = 8080;
 //const db = require("./db"); //These 2 files are for the database portion. */
 //const collection = "Antics"; 
-app.get('/', (req, res) => res.sendFile(__dirname+ "/Arthurs_Antics.html"));
+app.get('/', (req, res) => res.sendFile(__dirname + './Arthurs_Antics.html'));
 app.listen(port, () => console.log('Example app listening at http://localhost:8080'));
 
 app.get('/', function(req, res)    {
