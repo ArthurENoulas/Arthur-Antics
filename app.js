0fce6fd3f9b6e7cbf1e23c46e10c2b86c068b652
ArthurENoulas <arthurnoulas@gmail.com>
2024-02-17 20:50:15 -0500

    git push

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const path = require('path'); //created the path module
 const port = 8080;
 //const db = require("./db"); //These 2 files are for the database portion. */
 //const collection = "Antics"; 
-app.get('/', (req, res) => res.sendFile(__dirname + '/Arthurs_Antics.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/Arthur_Antics.html'));
 app.listen(port, () => console.log('Example app listening at http://localhost:8080'));
 
 app.get('/', function(req, res)    {
     res.sendFile(__dirname+ '/Antics.css');
 });
+
+
+//get connected to the pix folder
